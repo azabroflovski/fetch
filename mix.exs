ed640e6a681dc97c532564e41eb0ddc7b4ec9226
azabroflovski <azabroflovski@gmail.com>
2025-03-16 09:37:08 +0500

    chore: add lib info

--- mix.exs
+++ mix.exs
@@ -4,10 +4,14 @@ defmodule Fetch.MixProject do
   def project do
     [
       app: :fetch,
-      version: "0.1.0",
-      elixir: "~> 1.18",
+      version: "0.0.1",
+      elixir: "~> 1.0",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package(),
+      name: "Fetch",
+      source_url: "https://github.com/azabroflovski/fetch"
     ]
   end
 
@@ -23,6 +27,19 @@ defmodule Fetch.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp description() do
+    "🚀 A lightweight HTTP library inspired by JavaScript's fetch, bringing simplicity and flexibility to Elixir HTTP requests."
+  end
+
+  defp package() do
+    [
+      name: "fetch",
+      files: ~w(lib .formatter.exs mix.exs README*),
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/azabroflovski/fetch"}
     ]
   end
 end
