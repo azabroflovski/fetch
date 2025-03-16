3814a41c8ee51d00aed57bc922207590bd539fdc
azabroflovski <azabroflovski@gmail.com>
2025-03-16 09:46:05 +0500

    chore(deps): add ex_doc

--- mix.exs
+++ mix.exs
@@ -11,22 +11,21 @@ defmodule Fetch.MixProject do
       description: description(),
       package: package(),
       name: "Fetch",
-      source_url: "https://github.com/azabroflovski/fetch"
+      source_url: "https://github.com/azabroflovski/fetch",
+      homepage_url: "https://github.com/azabroflovski/fetch",
+      docs: &docs/0
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, "~> 0.34", only: :dev, runtime: false}
     ]
   end
 
@@ -40,6 +39,13 @@ defmodule Fetch.MixProject do
       files: ~w(lib .formatter.exs mix.exs README*),
       licenses: ["MIT"],
       links: %{"GitHub" => "https://github.com/azabroflovski/fetch"}
+    ]
+  end
+
+  defp docs do
+    [
+      main: "Fetch",
+      extras: ["README.md"]
     ]
   end
 end
